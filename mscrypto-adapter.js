bc1505737b172aa995fab3899ae13ac6153de525
Artem S Vybornov <vybornov@gmail.com>
2015-08-27 17:28:01 +0300

    import/export jwk keys fix

--- mscrypto-adapter.js
+++ mscrypto-adapter.js
@@ -34,15 +34,32 @@ self.crypto || !function () {
             return this.keyUsage || [];
         });
 
+        function s2b ( s ) {
+            var b = new Uint8Array(s.length);
+            for ( var i = 0; i < s.length; i++ ) b[i] = s.charCodeAt(i);
+            return b;
+        }
+
+        function b2s ( b ) {
+            if ( b instanceof ArrayBuffer ) b = new Uint8Array(b);
+            return String.fromCharCode.apply( String, b );
+        }
+
         [ 'decrypt', 'deriveBits', 'deriveKey', 'digest', 'encrypt', 'exportKey', 'generateKey' ,'importKey', 'sign', 'unwrapKey', 'verify', 'wrapKey' ]
             .forEach( function ( m ) {
                 var fn = _subtle[m];
-                _subtle[m] = function ( alg ) {
+                _subtle[m] = function ( a, b, c ) {
                     try {
-                        if ( m === 'decrypt' && alg.name.toUpperCase() === 'AES-GCM' ) {
-                            var tl = alg.tagLength >> 3, buff = arguments[2];
-                            arguments[2] = buff.slice( 0, buff.byteLength - tl ),
-                            alg.tag = buff.slice( buff.byteLength - tl );
+                        if ( m === 'decrypt' && a.name.toUpperCase() === 'AES-GCM' ) {
+                            var tl = a.tagLength >> 3;
+                            arguments[2] = c.slice( 0, c.byteLength - tl ),
+                            a.tag = c.slice( c.byteLength - tl );
+                        }
+
+                        if ( m === 'importKey' && a === 'jwk' && b.kty ) {
+                            b = JSON.stringify(b);
+                            b = unescape( encodeURIComponent(b) );
+                            arguments[1] = s2b(b).buffer;
                         }
 
                         var op = fn.apply( this, arguments );
@@ -58,6 +75,12 @@ self.crypto || !function () {
                                     r.set( new Uint8Array(c), 0 );
                                     r.set( new Uint8Array(t), c.byteLength );
                                     r = r.buffer;
+                                }
+
+                                if ( m === 'exportKey' && a === 'jwk' ) {
+                                    r = b2s(r);
+                                    r = decodeURIComponent( escape( r ) );
+                                    r = JSON.parse(r);
                                 }
 
                                 res(r);
